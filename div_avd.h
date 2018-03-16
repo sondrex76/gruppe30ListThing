1d24ed7aecef2f53b8c52cf0e88df833dc686c1b
sondrex <sondrewr@stud.ntnu.no>
2018-03-16 17:43:45 +0100

    La til siste manglende verdi til DivAvd

--- div_avd.h
+++ div_avd.h
@@ -3,14 +3,16 @@
 
 #include "ListTool2B.h"
 #include "lag.h"
+#include "resultat.h"
+#include "conster.h"
 
 // Er en divisjon/avdeling av en sport
 // Sortert etter navn av divisjon/avderling
 class DivAvd : TextElement {
 private:
-	int antLag;		// Antall lag i divisjon
-	Lag lag[30];	// Lagene i divisjonen
-	// Mangler to dimensjonal matrise av Resultat objekter for terminlisten av resultater
+	int antLag;								// Antall lag i divisjon
+	Lag lag[30];							// Lagene i divisjonen
+	Resultat* resultater[MAXLAG][MAXLAG];	// Matrise med pekere til resultater
 public:
 	DivAvd();
 };
